db7b4ae740672b7113bbeb4031a6dce8ea319287
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-29 22:11:57 +0300

    ...

---
+++ IdentityChatEmailNight/Models/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityChatEmailNight.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
+//Ali Yıldırım 123456 ali@gmail.com ali01
